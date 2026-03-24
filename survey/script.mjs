a16d0646945a8c33ae0de727efa7150a326c447b
joseph-yuruk <josephyuruk@gmail.com>
2026-03-24 16:51:37 -0700

    removal of survey

--- survey/script.mjs
+++ survey/script.mjs
@@ -1,4 +1,4 @@
-let params = new URLSearchParams(document.location.search);
-let ref = params.get("ref"); // is the string "Jonathan"
+//let params = new URLSearchParams(document.location.search);
+//let ref = params.get("ref"); // is the string "Jonathan"
 
-window.location.href = `https://yuruk.mfbeg.com/?ref=${ref === null ? "s" : ref}`;
+//window.location.href = `https://yuruk.mfbeg.com/?ref=${ref === null ? "s" : ref}`;
